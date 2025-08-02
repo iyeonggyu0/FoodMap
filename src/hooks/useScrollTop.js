5b4a3a135308cda5976e5b4413781eeb3ad40fbd
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-08-02 15:39:08 +0900

    Header, Footer 링크 수정

--- src/hooks/useScrollTop.js
+++ src/hooks/useScrollTop.js
@@ -3,11 +3,20 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (hash) {
+      // hash가 있을 때 해당 id로 스크롤 이동
+      const element = document.getElementById(hash.replace("#", ""));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      // hash가 없을 때만 맨 위로 이동
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null; // 아무 것도 렌더하지 않음
 };
