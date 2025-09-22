8bfc4810e186cdfb7bb823e2d49a6569536da68a
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-09-22 23:47:19 +0900

    이미지

--- src/components/MapPageCP/PcCP/index.jsx
+++ src/components/MapPageCP/PcCP/index.jsx
@@ -124,7 +124,7 @@ const PcCP = ({
         </div>
       </section>
       {onDetails && (
-        <PcCpDetailsStyle id="details" imgUrl={`${import.meta.env.VITE_API_URL}${details.imageUrl}`}>
+        <PcCpDetailsStyle id="details" imgUrl={`${details.imageUrl}`}>
           <p style={{ textAlign: "right", fontSize: "1.6rem" }}>
             <FontAwesomeIcon icon={faXmark} onClick={onDeleteDetails} className="icon" />
           </p>
