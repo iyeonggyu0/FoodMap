88e5085f103307ce5c84393a403bd864b0fd2796
Tim3208 <joungou.park@gmail.com>
2025-10-07 20:03:14 +0900

    feat(OutLineButtonCP): width 손쉽게 바꿀 수 있도록 프롭 추가

--- src/components/_common/OutLineButtonCP/index.jsx
+++ src/components/_common/OutLineButtonCP/index.jsx
@@ -6,11 +6,21 @@ const OutLineButtonCPStlye = styled.span`
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  padding: ${({ pcOnly, media }) => (pcOnly ? (media ? "10px 14px" : "unset") : "10px 14px")};
-  border-radius: ${({ pcOnly, media }) => (pcOnly ? (media ? "5px" : "unset") : "5px")};
-  color: ${({ pcOnly, media, color }) => (pcOnly ? (media ? color : "black") : color)};
-  border: ${({ pcOnly, media }) => (pcOnly ? (media ? "1px" : "0px") : "1px")} solid
-    ${({ borderColor, color }) => (borderColor ? (borderColor.startsWith("--") ? `var(${borderColor})` : borderColor) : color)};
+  padding: ${({ pcOnly, media }) =>
+    pcOnly ? (media ? "10px 14px" : "unset") : "10px 14px"};
+  border-radius: ${({ pcOnly, media }) =>
+    pcOnly ? (media ? "5px" : "unset") : "5px"};
+  color: ${({ pcOnly, media, color }) =>
+    pcOnly ? (media ? color : "black") : color};
+  border: ${({ pcOnly, media }) => (pcOnly ? (media ? "1px" : "0px") : "1px")}
+    solid
+    ${({ borderColor, color }) =>
+      borderColor
+        ? borderColor.startsWith("--")
+          ? `var(${borderColor})`
+          : borderColor
+        : color};
+  ${({ width }) => width && `width: ${width};`}
 `;
 
 /**
@@ -19,15 +29,39 @@ const OutLineButtonCPStlye = styled.span`
  * @param {ReactNode} icon 버튼 좌측에 표시할 아이콘 컴포넌트 (props: icon={<FontAwesomeIcon icon={faStar} />})
  * @param {boolean} [pcOnly=false] PC에서만 배경을 유지 할 예정인지 (기본값: F)
  * @param {string} color 글자와 테두리 색을 정한다. (기본값: #FFF)
- * @param {string} borderColor 테두리 색을 정한다. (기본값: X)
+ * @param {string} borderColor 테두리 색을 정한다. (기본값: "color"와 동일)
+ * @param {string} width 버튼의 너비를 정한다. (기본값: 자동)
  * @returns OutLineButtonCP 는 _common에 속하며, 해당 태그로 감싼 글자를 span으로 color의 테두리와 글자색을 만드는 css 적용
  */
 
-const OutLineButtonCP = ({ icon, children, pcOnly = false, color = "#FFF", borderColor }) => {
+const OutLineButtonCP = ({
+  icon,
+  children,
+  pcOnly = false,
+  color = "#FFF",
+  borderColor,
+  width,
+}) => {
   const isPc = useMedia().isPc;
   return (
-    <OutLineButtonCPStlye pcOnly={pcOnly} media={isPc} color={color} borderColor={borderColor}>
-      {icon && <span style={{ display: "inline-flex", alignItems: "center", marginRight: "0.5em" }}>{icon}</span>}
+    <OutLineButtonCPStlye
+      pcOnly={pcOnly}
+      media={isPc}
+      color={color}
+      borderColor={borderColor}
+      width={width}
+    >
+      {icon && (
+        <span
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            marginRight: "0.5em",
+          }}
+        >
+          {icon}
+        </span>
+      )}
       {children}
     </OutLineButtonCPStlye>
   );
